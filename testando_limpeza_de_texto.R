503a5df86befb3e8ffdcedb8b805352e7794a8ac
Fidel Alencar <parentedealencarpimentel@hotmail.com>
2020-07-07 17:42:37 -0300

    Acrescentando: (1) wordcloud; (2) qntdd das palavras mais frequentes; (3) lista de vocabulário do texto.

--- testando_limpeza_de_texto.R
+++ testando_limpeza_de_texto.R
@@ -15,24 +15,54 @@ V1 <- "https://www.youtube.com/watch?v=-LKVUarhtvE" # video sobre sabao e Covid-
 
 Legendas <- get_caption(url = V1, savexl = FALSE, openxl = FALSE, path = getwd()) # Puxando a legenda
 
-Legendas_vec <- pull(Legendas, text) # essa linha converte a coluna text de tibble para vetor
+Legendas_vec <- pull(Legendas4, text) # essa linha converte a coluna text de tibble para vetor
 NcharLegendas <- nchar(Legendas_vec) # numero de caracteres por linha
 Nwords_Legendas <- str_count(Legendas_vec, boundary("word")) # numero de palavras por linha
-TpalavrasLegendas <-sum(Nwords_Legendas) # qntdd total de palavras
+TpalavrasLegendas <- sum(Nwords_Legendas) # qntdd total de palavras
 
 Legendas_corpus <- Corpus(VectorSource(Legendas_vec)) # transformando vetor em corpus
-
-inspect(Legendas_corpus[22:27])
-
-
-Legendas_corpus <- tm_map(Legendas_corpus, tolower) # Aparentemente, as legendas nao veem com letra maiuscula
+inspect(Legendas_corpus[22:27]) # para ler textos do corpos
 
 
-Legendas_corpus <- tm_map(Legendas_corpus, removeNumbers)
+# Legendas_corpus <- tm_map(Legendas_corpus, tolower) # Aparentemente, as legendas nao veem com letra maiuscula
+
+# CONTINUAR ...
+
+
+Legendas_corpus <- tm_map(Legendas_corpus, removeNumbers) # Limpeza do corpus
 Legendas_corpus <- tm_map(Legendas_corpus, removePunctuation)
-Legendas_corpus <- tm_map(Legendas_corpus, removeWords, stopwords("english"))
+Legendas_corpus <- tm_map(Legendas_corpus, removeWords, stopwords("english")) 
+# exemplos de diferença da redução de palavras com e sem stopwords: 1455 -> 760  / 2514 -> 1357 / 2449 -> 1414
 Legendas_corpus <- tm_map(Legendas_corpus, stripWhitespace)
 Legendas_corpus <- tm_map(Legendas_corpus, stemDocument, language = "english")
+# Diferença de caracteres com e sem stemming: 5494 -> 4556  / 9881 -> 8271  / 9926 -> 8567
+
+
+# devolvendo para a classe vetor:
+legendas_limpas <- Legendas_corpus$content # vetor com as legendas limpas
+sum(nchar(legendas_limpas))  
+sum(str_count(legendas_limpas, boundary("word"))) 
+
+
+# para fazer "wordcloud"
+if(require(wordcloud) == F) install.packages("wordcloud"); require(wordcloud)
+wordcloud(Legendas_corpus, # fazendo a wordcloud com base no corpus
+          max.words = 100, # numero maximo de palavras da nuve,
+          random.order = T,
+          colors = rainbow(12), #numero de cores da paleta. testar colors = brewer.pal(8, "Dark2")
+          rot.per = 0.35, # proporcao de palavras em cada eixo(vertical/horizontal)
+          use.r.layout = T)
+
+
+
+# Para obter uma lista das palavras mais frequentes:
+# para ver como barras é preciso transformar em DTM, depois em matriz, depois em data.frame
+textsplitWord_dtm <- DocumentTermMatrix(Legendas_corpus)
+textsplitWordMatrix <- as.matrix(textsplitWord_dtm)
+textsplitWordSorted <- sort(colSums(textsplitWordMatrix), decreasing = T)
+textsplitWordDF <- data.frame(word = names(textsplitWordSorted), freq = textsplitWordSorted)
+textsplitWordDF[1:30,] # Para saber as 30 mais freqentes.
+
 
 
 # para saber o numero de palavras sem repetição
@@ -41,3 +71,6 @@ textsplitWordMatrix <- as.matrix(textsplitWord_dtm)
 textsplitWordSorted <- sort(colSums(textsplitWordMatrix), decreasing = T)
 textsplitWordDF <- data.frame(word = names(textsplitWordSorted), freq = textsplitWordSorted)
 NwordsLegendas <- count(textsplitWordDF)  # ___ palavras diferentes
+
+
+
