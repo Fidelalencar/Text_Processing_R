f8967d226a3fb22d751253d2b813dc242c1f7d2c
Fidel Alencar <parentedealencarpimentel@hotmail.com>
2020-10-15 17:44:36 -0300

    jogando o ultimo commit para dentro da funcao

--- Dados_Basicos_Legendas_nivel_Basico.R
+++ Dados_Basicos_Legendas_nivel_Basico.R
@@ -16,6 +16,29 @@ Dados_basicos_legenda <- function(Insira_Link_do_Video_aqui) {
   
   # Captura da legenda:
   if(require(youtubecaption) == F) install.packages("youtubecaption"); require(youtubecaption)
+  if(require(dplyr) == F) install.packages("dplyr"); require(dplyr)
+  if(require(RSQLite) == F) install.packages("RSQLite"); require(RSQLite)
+  if(require(tm) == F) install.packages("tm"); require(tm)
+  if(require(tokenizers) == F) install.packages("tokenizers"); require(tokenizers)
+  if(require(tidyverse) == F) install.packages("tidyverse"); require(tidyverse)
+  if(require(stringr) == F) install.packages("stringr"); require(stringr)
+
+  
+  
+  # Carregando a Lista de 5000 palavras ----
+  setwd("C:/Users/Paren/Dropbox/Udacity/LearningCommunityConteudo/listas_palavras") 
+  FiveThouMostFreq <- read.csv2("5000MostFreqWords.csv", sep = ";",  header = T)
+  # limpando, stemming e convertendo no formato adequado
+  FiveThouMostFreq <- Corpus(VectorSource(as.vector(FiveThouMostFreq$Word)))
+  FiveThouMostFreq <- tm_map(FiveThouMostFreq, stemDocument, language = "english")
+  FiveThouMostFreq <- tm_map(FiveThouMostFreq, stripWhitespace)
+  FiveThouMostFreq <- data.frame(matrix(unlist(FiveThouMostFreq$content), # Convertendo a lista em Data Frame
+                                        nrow=length(FiveThouMostFreq$content), byrow=F), stringsAsFactors=FALSE)
+  FiveThouMostFreq["words"] <- FiveThouMostFreq["matrix.unlist.FiveThouMostFreq.content...nrow...length.FiveThouMostFreq.content..."]
+  # mudando o nome da coluna
+  FiveThouMostFreq$matrix.unlist.FiveThouMostFreq.content...nrow...length.FiveThouMostFreq.content... <- NULL
+  FiveThouMostFreq$words <- str_replace(FiveThouMostFreq$words, " ", "") # ainda precisava limpar os espacos no inicio das strings
+    
   
   
   #Insira_Link_do_Video_aqui <- "https://www.youtube.com/watch?v=fK2IJ43ppd0"
@@ -25,21 +48,17 @@ Dados_basicos_legenda <- function(Insira_Link_do_Video_aqui) {
   # video sobre "slow aging": "https://www.youtube.com/watch?v=QRt7LjqJ45k"
   # video sobre sabao e Covid-19: "https://www.youtube.com/watch?v=-LKVUarhtvE"
   
-  
-  if(require(dplyr) == F) install.packages("dplyr"); require(dplyr)
-  if(require(RSQLite) == F) install.packages("RSQLite"); require(RSQLite)
+
   Legendas_vec <- pull(Legendas, start) # essa linha converte o tibble em vetor
   TdurationLegendas <- max(Legendas_vec) # ___ segundos = ___ minutos
   
   Legendas_string <- pull(Legendas, text) # essa linha converte o tibble em vetor
   NcharLegendas <- nchar(Legendas_string) # numero de caracteres por linha
   
-  library(stringr)
   Nwords_Legendas <- str_count(Legendas_string, boundary("word"))
   TpalavrasLegendas <-sum(Nwords_Legendas) # ___ palavras
   
   
-  if(require(tm) == F) install.packages("tm"); require(tm)
   Legendas_corpus <- Corpus(VectorSource(Legendas_string)) # criando corpus
   
   Legendas_corpus <- tm_map(Legendas_corpus, tolower)
@@ -48,6 +67,25 @@ Dados_basicos_legenda <- function(Insira_Link_do_Video_aqui) {
   Legendas_corpus <- tm_map(Legendas_corpus, removeWords, stopwords("english"))
   Legendas_corpus <- tm_map(Legendas_corpus, stripWhitespace)
   Legendas_corpus <- tm_map(Legendas_corpus, stemDocument, language = "english")
+ 
+  
+  # ajustando para comparar com o dicionario de 5 mil palavras
+  Legendas_corpus_limpo <- as.data.frame(Legendas_corpus$content)
+  Legendas_corpus_limpo <- map(Legendas_corpus_limpo, tokenize_words) # convertendo o dataframe em lista
+  Legendas_corpus_limpo <- data.frame(matrix(unlist(Legendas_corpus_limpo), # Convertendo a lista em Data Frame
+                                             nrow=length(Legendas_corpus_limpo), byrow=F), stringsAsFactors=FALSE)
+  Legendas_corpus_limpo <- map(Legendas_corpus_limpo, tokenize_words) # convertendo o dataframe em lista
+  Legendas_corpus_limpo <- data.frame(matrix(unlist(Legendas_corpus_limpo), # Convertendo a lista em Data Frame
+                                             nrow=length(Legendas_corpus_limpo), byrow=F), stringsAsFactors=FALSE)
+  # mudando o nome da coluna
+  Legendas_corpus_limpo["words"] <- Legendas_corpus_limpo["matrix.unlist.Legendas_corpus_limpo...nrow...length.Legendas_corpus_limpo..."]
+  Legendas_corpus_limpo$matrix.unlist.Legendas_corpus_limpo...nrow...length.Legendas_corpus_limpo... <- NULL
+  
+  # Fazendo o match e identificando quantas palvras existem em comum entre as legendas e o dicionario de 5 mil palavras 
+  Common_words <- inner_join(Legendas_corpus_limpo, FiveThouMostFreq) # encontrando as palavras que estao nas legendas e no dicionario
+  Common_words <- unique(Common_words)  # retirando as duplicatas
+  
+
   
   # para saber o numero de palavras sem repetição
   textsplitWord_dtm <- DocumentTermMatrix(Legendas_corpus)
@@ -56,12 +94,14 @@ Dados_basicos_legenda <- function(Insira_Link_do_Video_aqui) {
   textsplitWordDF <- data.frame(word = names(textsplitWordSorted), freq = textsplitWordSorted)
   NwordsLegendas <- count(textsplitWordDF)  # ___ palavras diferentes
   
-  #resultados : duracao / num de palavras / num de palavras diferentes
+  
+  #resultados : duracao / num de palavras / num de palavras diferentes / numero de palavras comuns ao dic de 5000
   TpalavrasLegendas  # ___ palavras
   NwordsLegendas     # ___  palavras diferentes
   TdurationLegendas  # ___ segundos
   ratioVocPorsec <- NwordsLegendas/TdurationLegendas # ___ palavras diferentes por segundo
   ratioPalavPorsec <- TpalavrasLegendas/TdurationLegendas # ___ palavras por segundo
+  n_commonwords <- as.numeric(count(Common_words)) # numero de palavras comuns ao dic de 5000
   
   N_de_Palavras <- round(as.numeric(TpalavrasLegendas), 1)
   Vocabulario <- round(as.numeric(NwordsLegendas), 1)
@@ -69,13 +109,16 @@ Dados_basicos_legenda <- function(Insira_Link_do_Video_aqui) {
   Vocabulario_PorSeg <- round(as.numeric(ratioVocPorsec), 2)
   Palavras_PorSeg <- round(as.numeric(ratioPalavPorsec), 2)
   
+  # 
+  
+  
   #rm("Insira_Link_do_Video_aqui", "Legendas", "Legendas_corpus", "Legendas_string", 
   #   "Legendas_vec", "N_de_Palavras", "NcharLegendas", "Nwords_Legendas", "NwordsLegendas", 
   #   "Palavras_PorSeg", "ratioPalavPorsec", "ratioVocPorsec", "TdurationLegendas", 
   #   "Tempo_minutos", "textsplitWord_dtm", "textsplitWordDF", "textsplitWordMatrix", "textsplitWordSorted", 
   #   "TpalavrasLegendas", "Vocabulario", "Vocabulario_PorSeg")
   
-  tabela <- as.data.frame(cbind(N_de_Palavras, Vocabulario, Tempo_minutos, Vocabulario_PorSeg, Palavras_PorSeg))
+  tabela <- as.data.frame(cbind(N_de_Palavras, Vocabulario, Tempo_minutos, Vocabulario_PorSeg, Palavras_PorSeg, n_commonwords))
   return(tabela)
 }
 
@@ -92,56 +135,6 @@ tabela <- as.data.frame(
 
 colnames(tabela) <- c("Número de Palavras", "Vocabulario", 
                       "Tempo em minutos", "Vocabulario por segundo", 
-                      "Palavras por segundo")
+                      "Palavras por segundo", "Palavras entre as 5 mil")
 
 ###########################################################
-
-
-if(require(dplyr) == F) install.packages("dplyr"); require(dplyr)
-if(require(RSQLite) == F) install.packages("RSQLite"); require(RSQLite)
-if(require(tm) == F) install.packages("tm"); require(tm)
-# baixando uma legenda
-Insira_Link_do_Video_aqui <- "https://www.youtube.com/watch?v=fK2IJ43ppd0"
-Legendas <- get_caption(url = Insira_Link_do_Video_aqui, savexl = FALSE, openxl = FALSE, path = getwd())
-# limpando, stemming e convertendo para o formato adequado
-Legendas_string <- pull(Legendas, text) # converte o tibble em vetor
-Legendas_corpus <- Corpus(VectorSource(Legendas_string)) # converte os vetores em corpus
-Legendas_corpus_limpo <- tm_map(Legendas_corpus, removeWords, stopwords("english")) # Limpando as stopwords
-Legendas_corpus_limpo <- tm_map(Legendas_corpus_limpo, removeNumbers)
-Legendas_corpus_limpo <- tm_map(Legendas_corpus_limpo, stripWhitespace)
-Legendas_corpus_limpo <- tm_map(Legendas_corpus_limpo, stemDocument, language = "english")
-# inspect(Legendas_corpus_limpo[1])
-Legendas_corpus_limpo <- as.data.frame(Legendas_corpus_limpo$content)
-if(require(tokenizers) == F) install.packages("tokenizers"); require(tokenizers)
-Legendas_corpus_limpo <- map(Legendas_corpus_limpo, tokenize_words) # convertendo o dataframe em lista
-Legendas_corpus_limpo <- data.frame(matrix(unlist(Legendas_corpus_limpo), # Convertendo a lista em Data Frame
-                                           nrow=length(Legendas_corpus_limpo), byrow=F), stringsAsFactors=FALSE)
-Legendas_corpus_limpo <- map(Legendas_corpus_limpo, tokenize_words) # convertendo o dataframe em lista
-Legendas_corpus_limpo <- data.frame(matrix(unlist(Legendas_corpus_limpo), # Convertendo a lista em Data Frame
-                                           nrow=length(Legendas_corpus_limpo), byrow=F), stringsAsFactors=FALSE)
-# mudando o nome da coluna
-Legendas_corpus_limpo["words"] <- Legendas_corpus_limpo["matrix.unlist.Legendas_corpus_limpo...nrow...length.Legendas_corpus_limpo..."]
-Legendas_corpus_limpo$matrix.unlist.Legendas_corpus_limpo...nrow...length.Legendas_corpus_limpo... <- NULL
-
-
-
-# Carregando a Lista de 5000 palavras ----
-if(require(tidyverse) == F) install.packages("tidyverse"); require(tidyverse)
-setwd("C:/Users/Paren/Dropbox/Udacity/LearningCommunityConteudo/listas_palavras") 
-FiveThouMostFreq <- read.csv2("5000MostFreqWords.csv", sep = ";",  header = T)
-# limpando, stemming e convertendo no formato adequado
-FiveThouMostFreq <- Corpus(VectorSource(as.vector(FiveThouMostFreq$Word)))
-FiveThouMostFreq <- tm_map(FiveThouMostFreq, stemDocument, language = "english")
-FiveThouMostFreq <- tm_map(FiveThouMostFreq, stripWhitespace)
-FiveThouMostFreq <- data.frame(matrix(unlist(FiveThouMostFreq$content), # Convertendo a lista em Data Frame
-                                      nrow=length(FiveThouMostFreq$content), byrow=F), stringsAsFactors=FALSE)
-FiveThouMostFreq["words"] <- FiveThouMostFreq["matrix.unlist.FiveThouMostFreq.content...nrow...length.FiveThouMostFreq.content..."]
-# mudando o nome da coluna
-FiveThouMostFreq$matrix.unlist.FiveThouMostFreq.content...nrow...length.FiveThouMostFreq.content... <- NULL
-FiveThouMostFreq$words <- str_replace(FiveThouMostFreq$words, " ", "") # ainda precisava limpar os espacos no inicio das strings
-
-
-# Fazendo o match e identificando quantas palvras existem em comum entre as legendas e o dicionario de 5 mil palavras 
-Common_words <- inner_join(Legendas_corpus_limpo, FiveThouMostFreq) # encontrando as palavras que estao nas legendas e no dicionario
-Common_words <- unique(Common_words)  # retirando as duplicatas
-
